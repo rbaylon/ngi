8a6438b2c4595a69cfcc1955beaaefab5fe0d1de
User Flaskapp <flaskapp@serv1.ibmsprint.xyz>
2021-11-26 23:53:31 +0800

    update live venv

--- start_sgi.sh
+++ start_sgi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-uwsgi -s /tmp/ngi_app.sock --manage-script-name --mount /=run:app --venv ~/.local/share/virtualenvs/biztool-zBknfHnG >/dev/null 2>&1 &
+uwsgi -s /tmp/ngi_app.sock --manage-script-name --mount /=run:app --venv ~/.local/share/virtualenvs/ngi-cO9TI7C3 >/dev/null 2>&1 &
 echo $! > /tmp/ngi_uwsgi_flaskapp.pid
